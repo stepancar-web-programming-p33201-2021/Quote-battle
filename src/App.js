bf538e69f2a1ccf89de3a322963e7784b9c8370b
Mikhail Sizov <mssizov@gmail.com>
2021-11-27 23:37:15 +0300

    Add component: quotes in statistic page

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import { View, ScreenSpinner, AdaptivityProvider, AppRoot, Button } from '@vkont
 import '@vkontakte/vkui/dist/vkui.css';
 
 import Bars from './panels/Statistics';
+import Panel from './panels/Panel';
+import StatQuote from './panels/StatQuote';
+import './App.css';
 
 const App = () => {
 	return (
 		<AdaptivityProvider>
 			<AppRoot>
-			<Bars values={[{name:"Cowboy", value:120}, 
-				{name:"Samurai", value:365},
-				{name:"Wolf", value:87},
-				{name:"Brat", value:60}]}></Bars>
+				<StatQuote quotes = {[{name:"Wolf", text:"Не тот волк, кто волк, а тот кто волк"}, 
+				{name:"Samurai", text:"Не бойся ножа, бойся вилки, один удар - четыре дырки"},
+				{name:"Cowboy", text:"Стреляй, а не болтай"},
+				{name:"Brat", text:"Стрелять будут по мне, а заденет вас"}]}></StatQuote>
 			</AppRoot>
 		</AdaptivityProvider>
 	);
